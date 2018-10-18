87bd3692f44b864717c2dccfc99aaee528e50415
MHelena45 <44232337+MHelena45@users.noreply.github.com>
2018-10-18 23:26:37 +0100

    Update comboios.cpp

--- src/comboios.cpp
+++ src/comboios.cpp
@@ -1,1 +1,32 @@
-//#include "comboios.h"
+#include "comboios.h"
+
+Comboio::Comboio(int horas, int minutos,int NumPasseiros) : NumMax (NumPasseiros), Horas (horas),Minutos (minutos) {
+	lotacao = 0;
+	ocupado = false;
+
+}
+int Comboio::getlotacao(){
+	return lotacao;
+}
+bool Comboio::getOcupado(){
+	if(NumMax == lotacao)
+		return true;
+	else return false;
+}
+bool Comboio::getMeioOcupado(){
+	if(lotacao > (NumMax /2))
+		return true;
+	else return false;
+}
+AlfaPendular::AlfaPendular(int horas, int minutos,int NumPasseiros) : Comboio(lotacao, horas, minutos){}
+string AlfaPendular::getInformacao(){
+	string Tipo = "AP";
+	return Tipo;
+}
+Intercidades::Intercidades(int HORAS, int MINUTOS,int NumeroPasseiros) : Comboio(HORAS,MINUTOS, NumeroPasseiros){}
+string Intercidades::getInformacao(){
+	string Tipo = "IC";
+	return Tipo;
+}
+
+
